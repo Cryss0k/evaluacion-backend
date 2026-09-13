99551ff83a9ee989d3a3091df6e22a68d9fd9a1e
Cryss0K <cristian.molina72@inacapmail.cl>
2026-09-12 23:26:12 -0300

    Se desarrolla app2 independiente con dos vistas HTML accesibles

---
+++ app2/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('vista3/', views.vista_tres, name='vista_tres'),
+    path('vista4/', views.vista_cuatro, name='vista_cuatro'),
+]
